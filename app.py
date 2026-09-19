051aa1a1a0c0f2903095a23897e7e1fa45a22c1b
Alireza <alirezaasady42@gmail.com>
2026-09-19 13:22:42 +0330

    feat(profile): add profile, account edit and avatars
    Closes #3

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 import os
 import logging
 from flask import Flask, request, url_for, redirect, render_template, flash, session
-from models import get_all_users__db, create_user__db, login_user__db
+from models import (
+    get_all_users__db,
+    create_user__db,
+    login_user__db,
+    get_user_by_id__db,
+    update_user__db,
+    delete_user__db,
+    create_profile__db,
+    get_profile__db,
+    update_profile__db,
+)
 from dotenv import load_dotenv
 from log_setup import get_logger
+from functools import wraps
 
 load_dotenv()
 
@@ -20,6 +31,18 @@ else:
     logger.propagate = False
 
 
+# ---------------- auth: access_control ----------------
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "user_id" not in session:
+            flash("ابتدا وارد شوید", "warning")
+            return redirect(url_for("auth_login"))
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
 # -------------------* HOME *--------------------------
 @app.route("/home")
 @app.route("/")
@@ -27,13 +50,13 @@ def home():
     return render_template("home.html")
 
 
-# ----------------* users *---------------------------
+# ----------------* USERS *---------------------------
 # ---------------- users: database -------------------
 @app.route("/users")
 def users_list():
     success, message, all_users = get_all_users__db()
     # print(success, message)
-    flash(message, "success")
+    flash(message, "success" if success else "error")
     # logger.info(f"info in users_list(message={message}")
     return render_template("users.html", users=all_users)
 
@@ -90,8 +113,7 @@ def auth_login():
             session["user_id"] = user_id
             flash(message, "success")
 
-            # TODO: redirect to edit_profile once
-            return redirect(url_for("auth_login"))
+            return redirect(url_for("edit_account"))
         else:
             flash(message, "error")
             return render_template("userslogin.html")
@@ -105,6 +127,148 @@ def auth_logout():
     session.pop("user_id", None)
     flash("exit!", "success")
     return redirect(url_for("users_list"))
+
+
+# -------------- users: edit account ------------
+@app.route("/users/account/edit", methods=["GET", "POST"])
+@login_required
+def edit_account():
+    user_id = session["user_id"]
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        firstname = request.form.get("firstname")
+        lastname = request.form.get("lastname")
+        age_raw = request.form.get("age")
+
+        if not username:
+            flash("نام کاربری را وارد کنید", "warning")
+            return redirect(url_for("edit_account"))
+
+        age = None
+        if age_raw:
+            try:
+                age = int(age_raw)
+            except ValueError:
+                flash("سن باید عدد باشد", "error")
+                return redirect(url_for("edit_account"))
+
+        success, message, result = update_user__db(
+            user_id, username, password, firstname, lastname, age
+        )
+        if success:
+            flash(message, "success")
+            return redirect(url_for("edit_account"))
+        else:
+            flash(f"{message} : خطا در بارگذاری اطلاعات", "error")
+            return redirect(url_for("edit_account"))
+
+    success, message, result = get_user_by_id__db(user_id)
+
+    if success:
+        # flash(message, "success")
+        return render_template("edit_account.html", user_data=result)
+    else:
+        flash(message, "error")
+        return render_template("edit_account.html", user_data=None)
+
+
+# ---------------- profile: management ----------------
+@app.route("/users/profile", methods=["GET", "POST"])
+@login_required
+def profile_manage():
+    user_id = session["user_id"]
+
+    if request.method == "POST":
+        bio = request.form.get("bio")
+        avatar_url = request.form.get("avatar_url")
+
+        success, message, existing_profile = get_profile__db(user_id)
+
+        if not success:
+            flash(message, "error")
+            return redirect(url_for("profile_manage"))
+
+        if existing_profile:
+            success, message = update_profile__db(
+                uid=user_id, bio=bio, avatar_url=avatar_url
+            )
+        else:
+            success, message = create_profile__db(
+                uid=user_id, bio=bio, avatar_url=avatar_url
+            )
+
+        flash(message, "success" if success else "error")
+        return redirect(url_for("profile_manage"))
+
+    success, message, result = get_profile__db(user_id)
+    if not success:
+        flash(message, "error")
+
+    return render_template("profile.html", profile=result)
+
+
+# -------------------# avatar #------------------------
+def get_avatar_list():
+    avatar_folder = os.path.join(app.static_folder, "avatars")
+    files = os.listdir(avatar_folder)
+    valid_extensions = (".jpg", ".jpeg", ".png", ".webp")
+    images = [f for f in files if f.lower().endswith(valid_extensions)]
+    return images
+
+
+# ---------------- profile: avatar ----------------
+@app.route("/users/profile/avatars", methods=["GET", "POST"])
+@login_required
+def choose_avatar():
+    if request.method == "POST":
+        avatar_filename = request.form.get("avatar_url")
+        valid_avatars = get_avatar_list()
+
+        if avatar_filename not in valid_avatars:
+            flash("آواتار انتخابی معتبر نیست", "error")
+            return redirect(url_for("choose_avatar"))
+
+        avatar_path = f"avatars/{avatar_filename}"
+
+        success, message, existing_profile = get_profile__db(session["user_id"])
+        if not success:
+            flash(message, "error")
+            return redirect(url_for("choose_avatar"))
+
+        if existing_profile:
+            success, message = update_profile__db(
+                uid=session["user_id"], avatar_url=avatar_path
+            )
+        else:
+            success, message = create_profile__db(
+                uid=session["user_id"], avatar_url=avatar_path
+            )
+
+        flash(message, "success" if success else "error")
+        return redirect(url_for("profile_manage"))
+
+    avatars = get_avatar_list()
+    return render_template("choose_avatar.html", avatars=avatars)
+
+
+# ---------------- profile: delete_account ----------------------
+@app.route("/users/profile/delete", methods=["POST"])
+@login_required
+def delete_account():
+    success, message = delete_user__db(session["user_id"])
+
+    if success:
+        session.clear()
+        flash(message, "success")
+        return redirect(url_for("auth_login"))
+    else:
+        flash(message, "error")
+        return redirect(url_for("profile_manage"))
+
+
+# -------------------------------------------------------
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,5 +1,14 @@
-from sqlalchemy import create_engine, Column, Integer, String, select, Text, DateTime
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    String,
+    select,
+    Text,
+    DateTime,
+    ForeignKey,
+)
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 from log_setup import get_logger
@@ -33,15 +42,31 @@ class User(Base):
     lastname = Column(String(50), nullable=True)
     age = Column(Integer, nullable=True)
 
+    a_profile = relationship(
+        "Profile", back_populates="a_user", uselist=False, cascade="all, delete-orphan"
+    )
+
     def __repr__(self):
         return f"<user id= {self.id}: username= {self.username} firstname= {self.firstname} lastname= {self.lastname} age= {self.age}>"
+
+
+# ---------------- models: profile --------------------
+class Profile(Base):
+    __tablename__ = "profiles"
+
+    id = Column(Integer, primary_key=True)
+    bio = Column(Text)
+    avatar_url = Column(String(255))
+
+    user_id = Column(Integer, ForeignKey("users.id"), unique=True, nullable=False)
+
+    a_user = relationship("User", back_populates="a_profile")
 
 
 # ----------------------------------------------------
 
 # ----------------*** Session Setup ***---------------
 # ---------------- app: database_session --------------
-# ساخت جدول‌ها در دیتابیس (فقط اگر وجود نداشته باشن)
 Base.metadata.create_all(engine)
 
 # ساخت session
@@ -52,17 +77,6 @@ session = SessionLocal()
 
 
 # -------------------* USERS *-------------------------
-# ---------------- users: list_all --------------------
-def get_all_users__db():
-    stmt = select(User.id, User.firstname, User.lastname, User.age)
-    try:
-        result = session.execute(stmt).all()
-    except Exception as e:
-        return False, f"Failed to fetch users: {e}", None
-    else:
-        return True, "users loaded successfully", result
-
-
 # ---------------- users: create ----------------------
 def create_user__db(username, password, firstname=None, lastname=None, age=None):
     try:
@@ -84,6 +98,33 @@ def create_user__db(username, password, firstname=None, lastname=None, age=None)
         return False, "Something went wrong, please try again"
 
 
+# ---------------- users: list_all --------------------
+def get_all_users__db():
+    stmt = select(User.id, User.firstname, User.lastname, User.age)
+    try:
+        result = session.execute(stmt).all()
+    except Exception as e:
+        return False, f"Failed to fetch users: {e}", None
+    else:
+        return True, "users loaded successfully", result
+
+
+# ---------------- users: delete ----------------------
+def delete_user__db(uid):
+    try:
+        user = session.query(User).filter(User.id == uid).one_or_none()
+        if not user:
+            return False, "کاربر پیدا نشد"
+
+        session.delete(user)
+        session.commit()
+        return True, "User deleted successfully"
+    except Exception as e:
+        session.rollback()
+        print(f"Error: {e}")
+        return False, "Something went wrong, please try again"
+
+
 # ---------------- users: login -----------------------
 def login_user__db(username, password):
     stmt = select(User).where(User.username == username)
@@ -96,3 +137,97 @@ def login_user__db(username, password):
         return True, "Login successful", result.id
     else:
         return False, "Invalid username or password", None
+
+
+# ---------------- users: show_one --------------------
+def get_user_by_id__db(uid):
+    stmt = select(User).where(User.id == uid)
+    result = session.execute(stmt).scalar_one_or_none()
+    if result is None:
+        return False, "user not fond", None
+    else:
+        return True, "Login user data successful", result
+
+
+# ---------------- users: update ----------------------
+def update_user__db(uid, username, password, firstname=None, lastname=None, age=None):
+    stmt = select(User).where(User.id == uid)
+    result = session.execute(stmt).scalar_one_or_none()
+    if result is None:
+        return False, "user not found", None
+
+    try:
+        result.username = username
+        if password:
+            result.password = generate_password_hash(password)
+        result.firstname = firstname
+        result.lastname = lastname
+        result.age = age
+        session.commit()
+        return True, "update successful", result
+    except IntegrityError:
+        session.rollback()
+        return False, "This username is already taken", None
+    except Exception as e:
+        session.rollback()
+        return False, "Something went wrong, please try again", None
+
+
+# -----------------------------------------------------
+
+
+# -------------------* PROFILE *-----------------------
+# ---------------- profile: create --------------------
+def create_profile__db(uid, bio=None, avatar_url=None):
+    try:
+        new_profile = Profile(bio=bio, avatar_url=avatar_url, user_id=uid)
+        session.add(new_profile)
+        session.commit()
+        return True, "Create profile successful"
+    except IntegrityError as e:
+        session.rollback()
+        print(f"Error: {e}")
+        return False, "شما از قبل پروفایل دارید"
+    except Exception as e:
+        session.rollback()
+        print(f"Error: {e}")
+        return False, "Something went wrong, please try again"
+
+
+# ---------------- profile: get -----------------------
+def get_profile__db(uid):
+    try:
+        profile = session.query(Profile).filter(Profile.user_id == uid).one_or_none()
+        if not profile:
+            return True, "پروفایلی ندارید", None
+        return True, "load_profile successful", profile
+    except Exception as e:
+        session.rollback()
+        print(f"Error: {e}")
+        return False, "Something went wrong, please try again", None
+
+
+# ---------------- profile: update --------------------
+def update_profile__db(uid, bio=None, avatar_url=None):
+    success, message, profile = get_profile__db(uid)
+    if not success:
+        return False, message, None
+
+    try:
+        if bio:
+            profile.bio = bio
+        if avatar_url:
+            profile.avatar_url = avatar_url
+
+        if bio or avatar_url:
+            session.commit()
+            return True, "update_profile successful"
+
+        return False, "update_profile Not successful"
+    except Exception as e:
+        session.rollback()
+        print(f"Error: {e}")
+        return False, "Something went wrong, please try again"
+
+
+# -----------------------------------------------------
